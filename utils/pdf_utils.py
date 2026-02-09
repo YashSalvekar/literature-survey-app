231b37008a30ae721bd934c0a2d50458d6d6153a
YashSalvekar <107533413+YashSalvekar@users.noreply.github.com>
2026-02-09 16:42:44 +0530

    Create pdf_utils.py

---
+++ utils/pdf_utils.py
@@ -0,0 +1,9 @@
+import fitz
+
+
+def extract_text_from_pdf_bytes(pdf_bytes):
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
