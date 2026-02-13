2882d8e60e683868405fcdf4a777e03e67cd856b
YashSalvekar <107533413+YashSalvekar@users.noreply.github.com>
2026-02-13 18:07:27 +0530

    Update file_utils.py
    update

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,4 +1,21 @@
-import io
+import zipfile
+import os
+from io import BytesIO
+
+def create_zip(file_paths):
+    zip_buffer = BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zf:
+        for path in file_paths:
+            zf.write(path, arcname=os.path.basename(path))
+
+    zip_buffer.seek(0)
+    return zip_buffer.getvalue()
+
+
+
+
+'''import io
 import zipfile
 
 
@@ -11,4 +28,4 @@ def create_zip(files_dict):
         for fname, fbytes in files_dict.items():
             zipf.writestr(fname, fbytes)
     buffer.seek(0)
-    return buffer
+    return buffer'''
